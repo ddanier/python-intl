93ffa9340253e714ef3f9a7e60239eeae0bdb990
David Danier <david.danier@gmail.com>
2026-08-22 09:29:46 +0200

    test: Add manual tests for format_to_parts

--- tests/test_datetimeformat.py
+++ tests/test_datetimeformat.py
@@ -11,6 +11,7 @@ from python_intl.datetimeformat import DateTimeFormatOptions
 if TYPE_CHECKING:
     from python_intl.datetimeformat import DateTimeFormatOptionsDictT
 
+
 @pytest.mark.parametrize(
     ("options", "expected"),
     [
@@ -28,7 +29,7 @@ if TYPE_CHECKING:
         ),
     ],
 )
-def test_options_to_json(options: DateTimeFormatOptionsDictT, expected: dict[str, str | int]):
+def test_options_to_json(options: DateTimeFormatOptionsDictT, expected: DateTimeFormatOptionsDictT):
     options = DateTimeFormatOptions(**options)
     assert options.to_json() == expected
 
@@ -51,6 +52,21 @@ def test_full_numeric_date(locale: str, expected: str):
 @pytest.mark.parametrize(
     ("locale", "expected"),
     [
+        ("en-US", [("month", "08"), ("literal", "/"), ("day", "15"), ("literal", "/"), ("year", "2026")]),
+        ("en-GB", [("day", "15"), ("literal", "/"), ("month", "08"), ("literal", "/"), ("year", "2026")]),
+        ("de-DE", [("day", "15"), ("literal", "."), ("month", "08"), ("literal", "."), ("year", "2026")]),
+        ("sv-SE", [("year", "2026"), ("literal", "-"), ("month", "08"), ("literal", "-"), ("day", "15")]),
+    ],
+)
+def test_full_numeric_date_parts(locale: str, expected: str):
+    datetime_ = dt.datetime(2026, 8, 15)
+    formatter = DateTimeFormat(locale, {"year": "numeric", "month": "2-digit", "day": "2-digit"})
+    assert [(part.type, part.value) for part in formatter.format_to_parts(datetime_)] == expected
+
+
+@pytest.mark.parametrize(
+    ("locale", "expected"),
+    [
         ("en-US", "August 15, 2026"),
         ("en-GB", "15 August 2026"),
         ("de-DE", "15. August 2026"),
@@ -66,6 +82,21 @@ def test_written_date(locale: str, expected: str):
 @pytest.mark.parametrize(
     ("locale", "expected"),
     [
+        ("en-US", [("month", "August"), ("literal", " "), ("day", "15"), ("literal", ", "), ("year", "2026")]),
+        ("en-GB", [("day", "15"), ("literal", " "), ("month", "August"), ("literal", " "), ("year", "2026")]),
+        ("de-DE", [("day", "15"), ("literal", ". "), ("month", "August"), ("literal", " "), ("year", "2026")]),
+        ("sv-SE", [("day", "15"), ("literal", " "), ("month", "augusti"), ("literal", " "), ("year", "2026")]),
+    ],
+)
+def test_written_date_parts(locale: str, expected: str):
+    datetime_ = dt.datetime(2026, 8, 15)
+    formatter = DateTimeFormat(locale, {"year": "numeric", "month": "long", "day": "numeric"})
+    assert [(part.type, part.value) for part in formatter.format_to_parts(datetime_)] == expected
+
+
+@pytest.mark.parametrize(
+    ("locale", "expected"),
+    [
         ("en-US", "Saturday, August 15"),
         ("en-GB", "Saturday 15 August"),
         ("de-DE", "Samstag, 15. August"),
@@ -76,3 +107,54 @@ def test_written_day_and_date(locale: str, expected: str):
     datetime_ = dt.datetime(2026, 8, 15)
     formatter = DateTimeFormat(locale, {"month": "long", "day": "numeric", "weekday": "long"})
     assert formatter.format(datetime_) == expected
+
+
+@pytest.mark.parametrize(
+    ("locale", "expected"),
+    [
+        (
+            "en-US",
+            [
+                ("weekday", "Saturday"),
+                ("literal", ", "),
+                ("month", "August"),
+                ("literal", " "),
+                ("day", "15"),
+            ],
+        ),
+        (
+            "en-GB",
+            [
+                ("weekday", "Saturday"),
+                ("literal", " "),
+                ("day", "15"),
+                ("literal", " "),
+                ("month", "August"),
+            ],
+        ),
+        (
+            "de-DE",
+            [
+                ("weekday", "Samstag"),
+                ("literal", ", "),
+                ("day", "15"),
+                ("literal", ". "),
+                ("month", "August"),
+            ],
+        ),
+        (
+            "sv-SE",
+            [
+                ("weekday", "lördag"),
+                ("literal", " "),
+                ("day", "15"),
+                ("literal", " "),
+                ("month", "augusti"),
+            ],
+        ),
+    ],
+)
+def test_written_day_and_date_parts(locale: str, expected: str):
+    datetime_ = dt.datetime(2026, 8, 15)
+    formatter = DateTimeFormat(locale, {"month": "long", "day": "numeric", "weekday": "long"})
+    assert [(part.type, part.value) for part in formatter.format_to_parts(datetime_)] == expected
